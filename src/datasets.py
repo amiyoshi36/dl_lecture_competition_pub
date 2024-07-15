aa47fa72927f3917899d88691824bbabc5225e79
amiyoshi36 <344akito@gmail.com>
2024-07-15 10:47:22 +0000

    load images from .pt files.

--- src/datasets.py
+++ src/datasets.py
@@ -50,11 +50,11 @@ class ThingsMEGDataset_2(torch.utils.data.Dataset):  # load images too
     def __init__(self, split: str, data_dir: str = "data") -> None:
         super().__init__()
 
-        self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),  # Resize images to a fixed size
-            transforms.ToTensor(),  # Convert PIL Image to PyTorch tensor
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 正規化
-        ])
+        #self.transform = transforms.Compose([
+        #    transforms.Resize((224, 224)),  # Resize images to a fixed size
+        #    transforms.ToTensor(),  # Convert PIL Image to PyTorch tensor
+        #    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 正規化
+        #])
         
         assert split in ["train", "val", "test"], f"Invalid split: {split}"
         self.split = split
@@ -65,35 +65,36 @@ class ThingsMEGDataset_2(torch.utils.data.Dataset):  # load images too
         
         if split in ["train", "val"]:
             self.y = torch.load(os.path.join(data_dir, f"{split}_y.pt"))
+            self.images = torch.load(os.path.join(data_dir, f"{split}_images.pt"))
             assert len(torch.unique(self.y)) == self.num_classes, "Number of classes do not match."
 
-        if split in ["train", "val"]:
-          completed_paths = []
+        #if split in ["train", "val"]:
+        #  completed_paths = []
+        #
+        #  with open(f'data/{split}_image_paths.txt', 'r') as f:
+        #    self.image_paths = f.read().splitlines()
+        #  
+        #  for path in self.image_paths:
+        #    if not "/" in path:  # フォルダ名が欠落していたら, ファイル名から推測して補完
+        #      #foldername = path.split("_")[0]
+        #      foldername = '_'.join(path.split("_")[:-1])
+        #      path = os.path.join(foldername+"/", path)
+        #    path = os.path.join("data/Images/", path)
+        #    completed_paths.append(path)
+        #  
+        #  self.image_paths = completed_paths
 
-          with open(f'data/{split}_image_paths.txt', 'r') as f:
-            self.image_paths = f.read().splitlines()
-          
-          for path in self.image_paths:
-            if not "/" in path:  # フォルダ名が欠落していたら, ファイル名から推測して補完
-              #foldername = path.split("_")[0]
-              foldername = '_'.join(path.split("_")[:-1])
-              path = os.path.join(foldername+"/", path)
-            path = os.path.join("data/Images/", path)
-            completed_paths.append(path)
-          
-          self.image_paths = completed_paths
 
-
-        # 画像のプリロード
-        self.images = []
-        for path in self.image_paths:
-            image = Image.open(path).convert("RGB")
-            image = self.transform(image)
-            self.images.append(image)
+        ## 画像のプリロード
+        #self.images = []
+        #for path in self.image_paths:
+        #    image = Image.open(path).convert("RGB")
+        #    image = self.transform(image)
+        #    self.images.append(image)
         
-        # save as tensor
-        tensor_path = os.path.join(data_dir, f"{split}_images.pt")
-        torch.save(self.images, tensor_path)
+        ## save as tensor
+        #tensor_path = os.path.join(data_dir, f"{split}_images.pt")
+        #torch.save(self.images, tensor_path)
           
 
     def __len__(self) -> int:
